446f9b2c129242584cf9c0b1aaec2c555eb344de
elena <lena.kazakova99@gmail.com>
2022-07-06 00:50:37 +0400

    Задача 2

---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// string num_str = Console.ReadLine();
+// int num = int.Parse(num_str);
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("понедельник");
+}
+else if (day == 2)
+{
+    Console.WriteLine("вторник");
+}
+else if (day == 3)
+{
+    Console.WriteLine("среда");
+}
+else if (day == 4)
+{
+    Console.WriteLine("четверг");
+}
+else if (day == 5)
+{
+    Console.WriteLine("пятница");
+}
+else if (day == 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (day == 7)
+{
+    Console.WriteLine("воскресенье");
+}
+else
+{
+    Console.WriteLine("нет такого дня");
+}
