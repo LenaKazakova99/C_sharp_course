fa3ba6e3ec273e9e17a1d15e87c527df6580edc2
elena <lena.kazakova99@gmail.com>
2022-07-06 00:16:01 +0400

    Задача 0

--- 1_lesson/1_1/Program.cs
+++ 1_lesson/1_1/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿// string num_str = Console.ReadLine();
+// int num = int.Parse(num_str);
+
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{num}^2: {num * num}");
