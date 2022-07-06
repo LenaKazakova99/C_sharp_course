4e6d4d243ae9ec3f3be02609debf86babb57986e
elena <lena.kazakova99@gmail.com>
2022-07-06 20:41:26 +0400

    ДЗ Задача 1

--- 1_lesson/HW1_1/Program.cs
+++ 1_lesson/HW1_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и выдаёт,
+//    какое число большее, а какое меньшее.
+
+string s_num1 = Console.ReadLine();
+string s_num2 = Console.ReadLine();
+
+int num1 = int.Parse(s_num1);
+int num2 = int.Parse(s_num2);
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше чем {num2}");
+}
+else if (num1 == num2)
+{
+    Console.WriteLine($"Число {num1} равно числу {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} меньше чем {num2}");
+}
