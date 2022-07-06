4101a8b9604c593f3ee84025b377aab487cc40de
elena <lena.kazakova99@gmail.com>
2022-07-07 00:40:58 +0400

    ДЗ Задача 2

---
+++ 1_lesson/HW1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая принимает на вход
+//    три числа и выдаёт максимальное из этих чисел.
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 < num2)
+{
+    num1 = num2;
+}
+if (num1 < num3)
+{
+    num1 = num3;
+}
+Console.Write($"Число {num1} является самым большим!");
