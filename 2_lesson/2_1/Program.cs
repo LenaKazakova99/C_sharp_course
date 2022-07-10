dc45acd54b996e3a8aaf2a757e716a9c4bb23ab1
elena <lena.kazakova99@gmail.com>
2022-07-10 16:01:07 +0400

    Задачи 1, 2, 3

---
+++ 2_lesson/2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+int TakeNum(int num)
+{
+    return num % 10;
+}
+
+Console.WriteLine(TakeNum(678));
+Console.WriteLine(TakeNum(6786784));
---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишит программу, которая будет принимать на вход два числа и выводить, является ли 
+// второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит 
+// остаток от деления.
+
+using System;
+
+namespace _2_2
+{
+    class Program
+    {
+        static int CheckinValves(in int val1, in int val2)
+        {
+            return(val1 % val2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите первое число");
+            int value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ведите второе число");
+            int value2 = int.Parse(Console.ReadLine());
+
+            int val = CheckinValves(in value1, in value2);
+
+            if (val == 0)
+            {
+                Console.WriteLine("Числа кратные");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток от деления {val}");
+            }
+        }
+    }
+}
