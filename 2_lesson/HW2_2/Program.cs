6c329a9909b51a1703633b19f709f7a3432377be
elena <lena.kazakova99@gmail.com>
2022-07-11 21:11:36 +0400

    hw Задача 2

---
+++ 2_lesson/HW2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2. Напишите программу, которая выводит случайное
+//    трёхзначное число и удаляет вторую цифру этого числа.
+
+void numRendom()
+{
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine($"{num} -> {num / 100}{num % 10}");  
+}
+
+NumRendom();
+
+void NumRendom()
+{   
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine($"{num} -> {num / 100 * 10 + num % 10}");        
+}
+
+NumRendom();
