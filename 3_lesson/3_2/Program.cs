c5c4c79628de426fd72d5632fb7f6cecfa315fbb
elena <lena.kazakova99@gmail.com>
2022-07-14 19:00:32 +0400

    Задача 2

--- 3_lesson/3_2/Program.cs
+++ 3_lesson/3_2/Program.cs
@@ -1,17 +1,9 @@
-﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон 
-// возможных координат точек в этой четверти (X и Y).
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве. 
 
-void Quarters(int num)
+double num(double x1, double x2, double y1, double y2)
 {
-    if (num == 1) Console.WriteLine("X > 0, Y > 0");
-    else if (num == 2) Console.WriteLine("X < 0, Y > 0");
-    else if (num == 3) Console.WriteLine("X < 0, Y < 0");
-    else if (num == 4) Console.WriteLine("X > 0, Y < 0");
-    else Console.WriteLine("не верно");
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
 }
-
-Quarters(1);
-Quarters(2);
-Quarters(3);
-Quarters(4);
-Quarters(7);
+num(3, 6, 9, 4);
+Console.WriteLine(num(3, 6, 9, 4));
