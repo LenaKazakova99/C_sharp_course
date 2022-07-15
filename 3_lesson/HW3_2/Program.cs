1554c43012a672f35e2299b4829647fd73ac15d3
elena <lena.kazakova99@gmail.com>
2022-07-15 19:27:10 +0400

    HW Задача 2

--- 3_lesson/HW3_2/Program.cs
+++ 3_lesson/HW3_2/Program.cs
@@ -6,5 +6,5 @@ double num(double x1, double x2, double y1, double y2, double z1, double z2)
 {
     return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
 }
-num(3, 6, 9, 4, 5, 7);
-Console.WriteLine(num(3, 6, 9, 4, 5, 7));
+num(3, 6, 9, 4, -2, 7);
+Console.WriteLine(num(3, 6, 9, 4, -2, 7));
