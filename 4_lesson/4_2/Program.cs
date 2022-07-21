371239baa50f5f6482d85b418235d199a34e740a
elena <lena.kazakova99@gmail.com>
2022-07-21 23:42:06 +0400

    4 lesson. Задача 2 для группы

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int MultNum(int N)
+{
+     int all_mult = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        all_mult *=i;
+    }
+    return all_mult;
+}
+Console.WriteLine(MultNum(4));
+Console.WriteLine(MultNum(5));
